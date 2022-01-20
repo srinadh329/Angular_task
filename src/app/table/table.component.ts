56a33bf09b66888f8690514f723682be1eecd1a1
siva srinadh <srinadh329@gmail.com>
2022-01-20 23:20:18 +0530

    update

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -8,11 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class TableComponent implements OnInit {
   student_details:any;
   data:any;
-  result:any;
-  marks_total:any;
   table_search:any;
-  update_details:any;
-  update_splice_details:any;
+  objIndex:any;
+  exit_details:any;
+  add_result:any;
   constructor() { }
 
   ngOnInit(): void {
@@ -22,26 +21,38 @@ export class TableComponent implements OnInit {
       {id:3, first:'siva',last:'srinadh',age:30,subj1_marks:100,subj2_marks:100,subj3_marks:100,subj4_marks:100,status:'pass'},
       {id:4, first:'siva',last:'srinadh',age:30,subj1_marks:100,subj2_marks:100,subj3_marks:100,subj4_marks:100,status:'pass'}
     ]
-    this.result = this.student_details.map((x:any)=>x.subj1_marks+x.subj2_marks+x.subj3_marks+x.subj4_marks)
-    this.result = this.result.reduce((a:any,b:any)=>{
-      return a+b
-    })
-    this.student_details['total']= this.result
-    console.log(this.student_details)
-    this.table_search = this.student_details
-    console.log(this.table_search)
+     // update function
 
-    this.update_details = this.student_details.find((x:any)=>x.id==2)
-    console.log(this.update_details)
-    this.update_splice_details = this.student_details.splice(this.update_details,1)
-    console.log(this.update_splice_details) 
-    this.update_splice_details = this.update_splice_details.map((x:any)=>[x.first='venkatesh',x.last='chavvakula'])
-    console.log(this.update_splice_details) 
-     this.student_details = [...this.update_splice_details,...this.student_details]
-     console.log(this.student_details)
+     this.objIndex = this.student_details.findIndex((x:any)=>x.id==2)
+     this.student_details[this.objIndex].first = 'venkatesh'
+     this.student_details[this.objIndex].last = 'ch'
+     console.log(this.objIndex)
+
+   // update function
+
+    // fullname and total marks
+    this.add_result = this.student_details.map((x:any)=>{
+      x.total=x.subj1_marks + x.subj2_marks+ x.subj3_marks+ x.subj4_marks
+      x.name = x.first + x.last
+      return(x)
+    })
+    console.log(this.add_result)  
+    console.log(this.student_details) 
+
+    // fullname and total marks
+
+
+   
+    // name exist for not
+      this.exit_details = this.student_details.every((x:any)=>x.name)
+      console.log(this.exit_details)
+      
+    
+    // name exist for not
+    
     }
   
-
+  // delete function  
   detele_item(student_details:any){
    let index_value = this.student_details.findIndex((x:any)=>x.id == student_details.id);
     console.log(index_value)
@@ -49,6 +60,9 @@ export class TableComponent implements OnInit {
       this.student_details.splice(index_value,1)
     }
   }
+  // delete function
+
+  // search function
   search_data(text:any){
     this.data = text.target.value
     console.log(this.data)
@@ -59,8 +73,11 @@ export class TableComponent implements OnInit {
       }
     })
   }
+  // search function
+  // column filter
   asyn(){
     this.student_details.sort((a:any,b:any)=>a.id-b.id) 
   }
+  // column filter
  
 }
